d8dfd4e32c66a1ad8f860e8947aa1ce8e4aa330f
pacientes <pacientes.jw@gmail.com>
2020-12-08 20:18:57 +0900

    Add Proxy Pattern

---
+++ 13.Proxy/Main.cpp
@@ -0,0 +1,21 @@
+﻿#include "Proxy.h"
+#include "RealSubject.h"
+
+int main(const int argc, const char* argv[])
+{
+#ifdef REMOTE_PROXY
+    std::shared_ptr<ProxyFactory> factory = std::make_shared<ProxyFactory>();
+    std::shared_ptr<Proxy> proxy = factory->getObject();
+
+    std::cout << proxy->action1() << std::endl;
+    std::cout << proxy->action2() << std::endl;
+#endif // REMOTE_PROXY
+
+#ifdef VIRTUAL_PROXY
+    std::shared_ptr<Proxy> proxy = std::make_shared<Proxy>();
+    proxy->action1();
+    proxy->action2();
+#endif
+
+    return 0;
+}
