681982997a240d011fba1f0e4d5474cb6bc59349
pacientes <pacientes.jw@gmail.com>
2020-12-09 22:56:37 +0900

    Add Visitor Pattern

---
+++ 16.Visitor/Cart.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Visitor.h"
+#include <iostream>
+
+class Cart :
+    public Element
+{
+public:
+    Cart(std::string name, int price, int num = 1) :
+        Element(name, price, num)
+    {
+    }
+
+    void accept(Visitor* visitor)
+    {
+        visitor->order(this);
+    }
+
+    int getTax(int tax = 10)
+    {
+        return (m_price * m_num) * tax / 100;
+    }
+
+    std::string list()
+    {
+        std::string order = m_name + ", num: " + std::to_string(m_num) + ", price: " + std::to_string(m_price * m_num) + ".\n";
+    }
+};
