ef51414f4bd4b4fe4db447bd56c2221ea56c0ddf
pacientes <pacientes.jw@gmail.com>
2020-12-14 03:53:38 +0900

    Add Mediator Pattern

---
+++ 19.Mediator/User.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "Common.h"
+#include <iostream>
+
+// ConcreteColleague
+class User :
+    public Colleague,
+    public std::enable_shared_from_this<User>
+{
+public:
+    User(std::shared_ptr<Mediator> mediator, std::string name) :
+        Colleague(mediator, name)
+    {
+
+    }
+
+    void sendMessage(std::string message)
+    {
+        std::cout << m_name + " [send] : " << message << std::endl;
+        m_mediator->sendMessage(message, shared_from_this());
+    }
+
+    void recvMessage(std::string message)
+    {
+        std::cout << m_name + " [recv] : " << message << std::endl;
+    }
+};
---
+++ 19.Mediator/chatMediator.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "Common.h"
+
+#include <vector>
+
+// ConcreteMediator
+class chatMediator :
+    public Mediator
+{
+public:
+    void addUser(std::shared_ptr<Colleague> colleague) override
+    {
+        m_colleagues.push_back(colleague);
+    }
+
+    void deleteUser(std::shared_ptr<Colleague> colleague) override
+    {
+        for (auto iter = m_colleagues.begin(); iter != m_colleagues.end(); iter++)
+        {
+            if (*iter == colleague)
+            {
+                m_colleagues.erase(iter);
+                break;
+            }
+        }
+    }
+
+    void sendMessage(std::string message, std::shared_ptr<Colleague> sender) override
+    {
+        for (const auto& colleague : m_colleagues)
+        {
+            if (colleague != sender)
+            {
+                colleague->recvMessage(message);
+            }
+        }
+    }
+
+private:
+    std::vector<std::shared_ptr<Colleague>> m_colleagues;
+};
