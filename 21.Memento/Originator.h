20098865b98d63bc14c8bffb9ac6bd0a47bd1649
pacientes <pacientes.jw@gmail.com>
2020-12-17 02:34:14 +0900

    Add Memento Pattern

---
+++ 21.Memento/Originator.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "Memento.h"
+#include <iostream>
+
+class Originator
+{
+public:
+    std::shared_ptr<Memento> createMemento()
+    {
+        return std::make_shared<Memento>(m_state);
+    }
+
+    void restore(std::shared_ptr<Memento> memento)
+    {
+        m_state = memento->getObject();
+    }
+
+    std::shared_ptr<Hello> getState() const
+    {
+        return m_state;
+    }
+
+    void setState(const std::shared_ptr<Hello> state)
+    {
+        // for deep-copy
+        m_state = std::make_shared<Hello>(*state);
+    }
+
+protected:
+    std::shared_ptr<Hello> m_state;
+};
