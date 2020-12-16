f126dd20c5b805f6b1dd023180b4ced7a3d637fa
pacientes <pacientes.jw@gmail.com>
2020-12-17 00:50:41 +0900

    add additional example
    light on / off

--- 20.State/Main.cpp
+++ 20.State/Main.cpp
@@ -6,6 +6,9 @@
 
 #include "Context.h"
 
+// Additional example
+#include "LightState.h"
+
 int main(const int argc, const char* argv[])
 {
     std::shared_ptr<Context> context = std::make_shared<Context>(std::make_shared<StateOrder>());
@@ -13,6 +16,9 @@ int main(const int argc, const char* argv[])
 
     context->setState(std::make_shared<StatePay>());
     context->request();
+
+    std::shared_ptr<LightLamp> lamp = std::make_shared<LightLamp>();
+    lamp->on();
 
     return 0;
 }
