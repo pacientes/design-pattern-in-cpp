f6ce9c1b03990b8d1cb44bd109f3cd7ee403de3a
pacientes <pacientes.jw@gmail.com>
2020-12-18 00:56:33 +0900

    Add Strategy Pattern

---
+++ 23.Strategy/Main.cpp
@@ -0,0 +1,18 @@
+﻿#include "Weapon.h"
+#include "Strategy.h"
+
+int main(const int argc, const char* argv[])
+{
+    std::shared_ptr<Charactor> charactor = std::make_shared<Charactor>();
+    charactor->attack();
+
+    std::shared_ptr<Knife> knife = std::make_shared<Knife>();
+    charactor->setWeapon(knife);
+    charactor->attack();
+
+    std::shared_ptr<Gun> gun = std::make_shared<Gun>();
+    charactor->setWeapon(gun);
+    charactor->attack();
+
+    return 0;
+}
---
+++ 23.Strategy/Strategy.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Weapon.h"
+#include <memory>
+
+class Strategy
+{
+public:
+    void setWeapon(std::shared_ptr<Weapon> weapon)
+    {
+        m_weapon = weapon;
+    }
+
+    virtual void attack() = 0;
+
+protected:
+    std::shared_ptr<Weapon> m_weapon;
+};
+
+class Charactor :
+    public Strategy
+{
+public:
+    void attack()
+    {
+        if (m_weapon == nullptr)
+        {
+            std::cout << "[Charactor] attack() hand" << std::endl;
+        }
+        else
+        {
+            m_weapon->attack();
+        }
+    }
+};
