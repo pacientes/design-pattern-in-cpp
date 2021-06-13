a4c9edd2bf575effd0c57176d3e527bfd5de9402
pacientes <pacientes.jw@gmail.com>
2021-06-13 23:03:20 +0900

    불필요한 주석 삭제

--- 04.AbstractFactory/Main.cpp
+++ 04.AbstractFactory/Main.cpp
@@ -21,10 +21,5 @@ int main(const int argc, const char* argv[])
     std::shared_ptr<ITireProduct> uTire = factory->createTire();
     uTire->makeAssemble();
 
-    // 추상 클래스는 그룹을 나눠서 복수의 객체를 생성하는데 편리하다. (Door, Tire)
-    // 그러나 새로운 객체를 추가한다면(ex, Engine)
-    // IFactory, KoreaFactory, StateFactory에 생성 로직을...
-    // IEngine, KoreaEngine, StateEngine을 추가해야하며, 이는 쉽지 않은 일이다.
-
     return 0;
 }
