82050651976ded86c9dfce964dcd5791e1b4ab64
pacientes <pacientes.jw@gmail.com>
2021-12-18 22:37:43 +0900

    VS2022 오류 수정

--- 13.Proxy/RemoteProxy.cpp
+++ 13.Proxy/RemoteProxy.cpp
@@ -11,7 +11,6 @@ RemoteProxy::RemoteProxy(std::shared_ptr<Subject> subject)
 std::string RemoteProxy::actionA()
 {
     std::cout << "[RemoteProxy] actionA doing" << std::endl;
-    // 다른 행동으로 변경 가능
     return "Replaced Remote Proxy ActionA()";
 }
 
@@ -19,12 +18,11 @@ std::string RemoteProxy::actionB()
 {
     std::cout << "[RemoteProxy] actionB doing" << std::endl;
 
-    // 결과에 대한 별도 처리 수행 가능
     std::string message = m_subject->actionB();
     if (message.empty())
     {
         return "Nothing returned by RealSubject actionB()...";
     }
 
-    return message;
+    return "";
 }
